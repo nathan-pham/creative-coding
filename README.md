69699289d31a23fc208dd995feec05f84f3ced58
Nathan Pham <nathanpham.me@gmail.com>
2022-07-31 17:16:58 -0700

    build sketches option for ease of use

--- README.md
+++ README.md
@@ -1,27 +1,3 @@
 # Creative Coding
 
 Random experiments, from generative art to interactive components
-
-## Sketches
-
--   [Sketch 01: Atomic Fishes](sketch_01.html)
--   [Sketch 02: Drawn Lines](sketch_02.html)
--   [Sketch 03: Circular Death](sketch_03.html)
--   [Sketch 04: Pyramids](sketch_04.html)
--   [Sketch 05: Shifting Sands](sketch_05.html)
--   [Sketch 06: Returning Spheres](sketch_06.html)
--   [Sketch 07: Asteroid](sketch_07.html)
--   [Sketch 08: Creative Link](sketch_08.html)
--   [Sketch 09: Slick Link](sketch_09.html)
--   [Sketch 10: Origin Lines](sketch_10.html)
--   [Sketch 11: Landscape](sketch_11.html)
--   [Sketch 12: Beautiful Flow Fields](sketch_12.html)
--   [Sketch 13: Sunset](sketch_13.html)
--   [Sketch 14: Fluid Grid](sketch_14.html)
--   [Sketch 15: Camo](sketch_15.html)
--   [Sketch 16: Waavy Input](sketch_16.html)
--   [Sketch 17: Rainbow Road](sketch_17.html)
--   [Sketch 18: Depth Lines](sketch_18.html)
--   [Sketch 19: Scales](sketch_19.html)
--   [Sketch 20: Galaxy](sketch_20.html)
--   [Sketch 21: Roe v Wade](sketch_21.html)
--- buildSketches.js
+++ buildSketches.js
@@ -1,23 +1,49 @@
-// const sketches = [
-//     "Atomic Fishes",
-//     "Drawn Lines",
-//     "Water Flower",
-//     "Pyramids",
-//     "Shifting Sands",
-//     "Returning Spheres",
-//     "Asteroid",
-//     "Creative Link",
-//     "Slick Link",
-//     "Origin Lines",
-//     "Landscape",
-//     "Flow Fields",
-//     "Sunset",
-//     "Fluid Grid",
-//     "Camo",
-//     "Waavy Input",
-//     "Rainbow Road",
-//     "Depth Lines",
-//     "Scales",
-//     "Galaxy",
-//     "Roe v Wade",
-// ];
+const fs = require("fs");
+
+const separateSketch = (name) => {
+    const capitalizeArray = (title) =>
+        title
+            .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+            .join(" ")
+            .replace(".html", "");
+
+    const [number, ...title] = name.split("_");
+    return [number, capitalizeArray(title)];
+};
+
+const buildSketches = () => {
+    const sketchesDir = "sketches";
+    const sketches = fs.readdirSync(sketchesDir);
+
+    const html = [];
+
+    for (const sketch of sketches) {
+        const [number, name] = separateSketch(sketch);
+
+        html.push(`
+            <li class="wrapper__list__item">
+            <a href="/sketches/${sketch}" target="_blank">
+                Sketch ${number.padStart(3, "0")}: ${name}
+            </a>
+            </li>
+        `);
+    }
+
+    const indexContent = fs.readFileSync("index.html", "utf-8");
+
+    const startTag = `<ul class="wrapper__list">`;
+    const endTag = `</ul>`;
+
+    const startIndex = indexContent.indexOf(startTag);
+    const endIndex = indexContent.indexOf(endTag);
+
+    const compiledIndexContent = `
+        ${indexContent.slice(0, startIndex + startTag.length)}
+        ${html.join("\n")}
+        ${indexContent.slice(endIndex)}
+    `;
+
+    fs.writeFileSync("index.html", compiledIndexContent);
+};
+
+buildSketches();
